5f1334bed5bd024ca8a44ef2cc0f5b4a291202e9
coldJanuary <145337363+coldJanuary@users.noreply.github.com>
2023-09-20 00:36:31 +0300

    homework_1st_lesson_task_1.py

--- homework_1st_lesson_task_1.py
+++ homework_1st_lesson_task_1.py
@@ -1,3 +1,3 @@
-a, b = map(int, input().split())  # пользователь вводит два числа через пробел
-print(a + b)
-print(a * b)
+x, y = map(int, input().split())  # пользователь вводит два числа через пробел
+print(x + y)
+print(x * y)
