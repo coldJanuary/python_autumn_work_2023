501fef1332a195f8dc0a45f3f4f1f367ce130017
ColdJanuary <bboxvad@mail.ru>
2023-09-20 00:27:27 +0300

    task 1.1

---
+++ homework_1st_lesson_task_1.py
@@ -0,0 +1,3 @@
+a,b=map(int, input().split()) # пользователь вводит два числа через пробел
+print(a+b)
+print(a*b)
--- task_1_1.py
+++ task_1_1.py
@@ -1,1 +1,7 @@
-print('hello world')
+a = input()
+b= input()
+if a>b:
+    print(a)
+elif a>b: print(b)
+else:
+    print('числа равны')
